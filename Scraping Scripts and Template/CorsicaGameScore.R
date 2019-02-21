0ca7e882a35a8086adeb14cb2d753077b0964d21
braydentang1 <brayden.tang1@gmail.com>
2019-02-21 13:03:47 -0600

    add more variables
    TOI% QoT, TOI% QoC

--- Scraping Scripts and Template/CorsicaGameScore.R
+++ Scraping Scripts and Template/CorsicaGameScore.R
@@ -54,7 +54,7 @@ getData = function(year){
   data = data %>%
               group_by(Team) %>%
               summarise_at(funs(mean(., na.rm = TRUE), sd(., na.rm=TRUE), min(.,na.rm=TRUE), max(., na.rm=TRUE)), .vars = c("iCF/60", "P1", "P1/60", "GS", "GS/60", "CF", "Rel CF%",
-                                                                                                                  "ixGF/60", "ZSR", "CF% QoT", "CF% QoC")) %>%
+                                                                                                                  "ixGF/60", "ZSR", "CF% QoT", "CF% QoC", "TOI% QoT", "TOI% QoC")) %>%
               mutate(Team = as.character(Team))
   
   bind_cols(data, tibble(Year = rep(year, nrow(data))))
