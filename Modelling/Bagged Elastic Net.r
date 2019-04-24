787cb5dc887053b82ddf1bfefb5636b5a704cda8
braydentang1 <brayden.tang1@gmail.com>
2019-04-23 20:41:35 -0600

    Update Bagged Elastic Net.r

--- Modelling/Bagged Elastic Net.r
+++ Modelling/Bagged Elastic Net.r
@@ -241,10 +241,8 @@ preProcess.recipe = function(trainX){
   
   mainRecipe = recipe(ResultProper ~., data=trainX) %>%
     step_dummy(all_predictors(), -all_numeric()) %>%
-    step_zv(all_predictors()) %>%
     step_interact(terms = ~ SRS:Fenwick:ELORating) %>%
     step_interact(terms = ~ H2H:VegasOpeningOdds) %>%
-    step_interact(terms = ~ RegularSeasonWinPercentage:contains("Points")) %>%
     step_interact(terms = ~ FaceoffWinPercentage:ShotPercentage) %>%
     step_interact(terms = ~ contains("Round"):VegasOpeningOdds) %>%
     step_interact(terms = ~ SDRecord:SOS) %>%
