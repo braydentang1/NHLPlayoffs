0aff042cf644a68ac3afdc76285cfdec51941317
brayden tang <brayden.tang1@gmail.com>
2019-10-20 17:16:51 -0700

    finish scripts.

--- src/prediction/R/final-model.R
+++ src/prediction/R/final-model.R
@@ -5,150 +5,135 @@ library(caret)
 library(parallel)
 library(ParBayesianOptimization)
 
-source("/home/brayden/GitHub/NHLPlayoffs/Prediction Scripts/All Required Functions For Final Model.R")
-setwd("/home/brayden/GitHub/NHLPlayoffs/Prediction Scripts/")
+source("src/prediction/R/final-model_required-functions.R")
 
 #..................................Read data in....................................#
-#Change directories to pull in data from the "Required Data Sets" folder located in the repository.
 
 cat("Reading in Data..... \n")
-allData = read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/HockeyReference2.csv") %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/HockeyReference1.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/CorsicaAllTeamStats.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/CorsicaGameScoreStats.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/ELORatings.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/ESPNStats.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/FenwickScores.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/NHLOfficialStats.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/SCFScores.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/VegasOddsOpening.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/EvolvingHockey_WAR.csv")) %>%
-  bind_cols(read_csv("/home/brayden/GitHub/NHLPlayoffs/Required Data Sets/TimeRelatedPlayoffFeatures.csv")) %>%
-  mutate(ResultProper = as.factor(ResultProper))
+all_data <- read_csv("data/processed/2006-2019_hockey-reference_other.csv") %>%
+  bind_cols(read_csv("data/processed/2006-2019_hockey-reference_aggregated.csv")) %>%
+  bind_cols(read_csv("data/processed/2008-2019_corsica_all-team-stats_processed.csv")) %>%
+  bind_cols(read_csv("data/processed/2008-2019_corsica_game-score.csv")) %>%
+  bind_cols(read_csv("data/processed/2006-2019_elo-ratings.csv")) %>%
+  bind_cols(read_csv("data/processed/2006-2019_espn_stats.csv")) %>%
+  bind_cols(read_csv("data/processed/2008-2019_naturalstattrick_scf.csv")) %>%
+  bind_cols(read_csv("data/processed/2006-2019_nhl-official.csv")) %>%
+  bind_cols(read_csv("data/processed/2006-2019_puck-on-net_last20.csv")) %>%
+  bind_cols(read_csv("data/processed/2006-2019_oddsportal.csv")) %>%
+  bind_cols(read_csv("data/processed/2008-2019_evolving-hockey_WAR.csv")) %>%
+  bind_cols(read_csv("data/processed/2008-2019_naturalstattrick_time-related.csv")) %>%
+  mutate(result_factor = as.factor(result_factor)) %>%
+  filter(!is.na(result_factor)) %>%
+  select(-series) %>%
+  set_names(~ str_to_lower(.) %>%
+              str_replace_all(" ", "_") %>%
+              str_replace_all("%", "_percent") %>%
+              str_replace_all("/", "_per"))
+
 
 #...................................Engineering of some features..................#
 
-allData = allData %>% 
-  mutate(Round = as.factor(c(rep(c(1,1,1,1,1,1,1,1,2,2,2,2,3,3,4),14)))) %>%
-  mutate(PenaltyMinstoPowerPlaylog = sign(PenaltyMinsPG*60*82 /PowerPlayPercentage) * log(abs(PenaltyMinsPG*60*82 /PowerPlayPercentage) + 1)) %>%
-  mutate(Ratio_of_SRStoPoints = (SRS/Points)^1/3) %>%
-  mutate(PowerPlaytoPenaltyKill = sign(PowerPlayPercentage/PenaltyKillPercentage) * log(abs(PowerPlayPercentage/PenaltyKillPercentage) + 1)) %>%
-  mutate(PPO_x_PenaltyKill = PowerPlayOppurtunities * PenaltyKillPercentage) %>%
-  mutate(GS_max_log = sign(GS_mean) * log(abs(GS_mean) + 1)) %>%
-  mutate(CA_Per60Team_log = sign(CA_Per60Team) * log(abs(CA_Per60Team) + 1)) %>%
-  mutate(Ratio_of_GoalstoGoalsAgainstlog = sign(GoalsFor/GoalsAgainst) * log(abs(GoalsFor/GoalsAgainst) +1)) %>%
-  mutate(Ratio_of_HitstoBlockslog = sign(HitsatES/BlocksatES) * log(abs(HitsatES/BlocksatES) + 1)) %>%
-  mutate(SCFtoGoalsAgainstlog = sign(SCF/GoalsAgainst) * log(abs(SCF/GoalsAgainst) + 1)) %>%
-  mutate(CorsiDifftoSOSlog = sign((CF_Per60Team - CA_Per60Team)/SOS) * log(abs((CF_Per60Team - CA_Per60Team)/SOS) + 1)) %>%
-  mutate(xGDifftoSOS = (xGF.60 - xGA.60)/SOS) %>% 
-  mutate(GStoSOS = GS_mean / SOS) %>%
-  mutate(SRStoSOS = SRS/SOS) %>%
-  mutate("ixGF/60_max.TO.Rel CF%_max" = allData$'Rel CF%_max' / allData$'ixGF/60_max') %>%
-  mutate_if(is.numeric, funs(ifelse(is.nan(.), 0,.))) %>%
-  mutate_if(is.numeric, funs(ifelse(is.infinite(.), 0,.)))
-
+all_data <- all_data %>% 
+  mutate(round = as.factor(c(rep(c(1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4), 14)))) %>%
+  mutate(penalty_mins_to_powerplay_log = sign(penalty_mins_pg * 60 * 82 / powerplay_percentage) * log(abs(penalty_mins_pg * 60 * 82 / powerplay_percentage) + 1)) %>%
+  mutate(ratio_of_srs_to_points = (srs / points)^1/3) %>%
+  mutate(power_play_to_penaltykill = sign(powerplay_percentage / penaltykill_percentage) * log(abs(powerplay_percentage / penaltykill_percentage) + 1)) %>%
+  mutate(ppo_x_penaltykill = powerplay_oppurtunities * penaltykill_percentage) %>%
+  mutate(gs_max_log = sign(gs_mean) * log(abs(gs_mean) + 1)) %>%
+  mutate(ca_per60team_log = sign(ca_per60team) * log(abs(ca_per60team) + 1)) %>%
+  mutate(ratio_of_goals_to_goals_against_log = sign(goals_for / goals_against) * log(abs(goals_for / goals_against) +1)) %>%
+  mutate(ratio_of_hits_to_blocks_log = sign(hits_at_es / blocks_at_es) * log(abs(hits_at_es / blocks_at_es) + 1)) %>%
+  mutate(scf_to_goalsagainst_log = sign(scf / goals_against) * log(abs(scf / goals_against) + 1)) %>%
+  mutate(corsidiff_to_sos_log = sign((cf_per60team - ca_per60team) / sos) * log(abs((cf_per60team - ca_per60team) / sos) + 1)) %>%
+  mutate(xgdiff_to_sos = (xgf_60 - xga_60) / sos) %>% 
+  mutate(gs_to_sos = gs_mean / sos) %>%
+  mutate(srs_to_sos = srs / sos) %>%
+  mutate(rel_cf_max_to_ixgf60_max = rel_cf_percent_max / ixgf_per60_max) %>%
+  mutate_if(is.numeric, .funs = list(~ifelse(is.nan(.), 0, .))) %>%
+  mutate_if(is.numeric, .funs = list(~ ifelse(is.infinite(.), 0, .))) 
 
 #..................................Separate out the new observations from all prior data........................#
 
-newdata = allData %>% filter(is.na(ResultProper))
-allData = allData %>% filter(!is.na(ResultProper))
+new_data <- all_data %>% filter(is.na(result_factor))
+all_data <- all_data %>% filter(!is.na(result_factor))
 
 #...........................Preprocess all the data; give the recipe parameters for all newdata........#
 
-preProcessData = function(data){
+pre_process_data <- function(data) {
   
-  ############################################################################################
-  # Preprocesses the data given in the argument "data" using the recipe defined in the function preProcess.recipe
-  # 
-  # Arguments:
-  #
-  # data -- a tibble containing the dataset that the final model will be fit on (i.e. the entire dataset)
-  #
-  # Returns:
-  #
-  # list
-  #   A list containing the preprocessed dataset and learned recipe parameters.
-  #
-  ############################################################################################
+  recipe_parameters <- prep(pre_process_recipe(data), training = data)
   
+  processed_data_set <- bake(recipe_parameters, new_data = data) 
   
-  recipe.parameters = prep(preProcess.recipe(data), training = data)
-  
-  processedDataSet = bake(recipe.parameters, new_data = data) 
-  
-  list(Data = processedDataSet, recipeParameters = recipe.parameters)
+  list(data = processed_data_set, recipe_parameters = recipe_parameters)
 }
 
 #...........................Prediction Script.................................................................................#
 #Requires new data samples. 
 
-predict.NHL = function(processedDataSet, recipeParameters, newdata, finalParameters){
+predict_NHL <- function(processed_data_set, recipe_parameters, new_data, final_parameters, times = 20) {
   
-  ############################################################################################
-  # Fits an ensemble of bagged elastic net models and combines their predictions using a simple average.
-  # 
-  # Arguments:
-  #
-  # processedDataSet -- a tibble of training data which should be the result of calling the function preProcessData
-  # recipeParameters -- a recipe which should be the result of calling the function preProcessData
-  # newdata -- a tibble of new data samples. Must contain the same predictors as the processedDataSet.
-  # finalParameters -- a tibble of hyperparameters to be used for each bagged elastic net in the ensemble; must contain column names alpha and lambda.
-  #
-  # Returns:
-  #
-  # list
-  #   A list containing predictions on newdata (given as probability of W = winning) and variable importance scores from the fitted model.
-  #
-  ############################################################################################
+  new_data <- new_data %>% 
+    select(-result_factor) %>% 
+    bake(recipe_parameters, new_data = .)
   
-  newdata = newdata %>% select(-ResultProper)
-  newdata = bake(recipeParameters, new_data = newdata)
+  frames_with_pca <- add_pca_variables(train_data = processed_data_set, test_data = new_data, standardize = FALSE)
   
-  frameswithPCA = addPCA_variables(traindata = processedDataSet, testdata = newdata, standardize = FALSE)
+  processed_data_set <- frames_with_pca$train
+  new_data <- frames_with_pca$test
   
-  processedDataSet = frameswithPCA$train
-  newdata = frameswithPCA$test
+  rm(frames_with_pca)
   
-  rm(frameswithPCA)
+  frames_with_knn <- add_knn_variables(train_data = processed_data_set, test_data = new_data, distances = TRUE, include_PCA = FALSE)
   
-  frameswithKNN = addKNN_variables(traindata = processedDataSet, testdata = newdata, distances = TRUE, include_PCA = FALSE)
+  processed_data_set <- frames_with_knn$train
+  new_data <- frames_with_knn$test
   
-  processedDataSet = frameswithKNN$train
-  newdata = frameswithKNN$test
+  rm(frames_with_knn)
   
-  rm(frameswithKNN)
+  predictions <- vector("list", nrow(final_parameters))
+  var_imp <- vector("list", nrow(final_parameters))
   
-  predictions = vector("list", nrow(finalParameters))
-  varImp = vector("list", nrow(finalParameters))
-  
-  for (i in 1:nrow(finalParameters)){
+  for (i in 1:nrow(final_parameters)) {
     
-    model = baggedModel(train = processedDataSet, test = newdata, label_train = processedDataSet$ResultProper, alpha.a = finalParameters$alpha[i], s_lambda.a = finalParameters$lambda[i], calibrate = FALSE)
-    predictions[[i]] = model$Predictions
-    varImp[[i]] = model$VariableImportance
+    model <- bagged_model(
+      train = processed_data_set,
+      test = new_data, 
+      label_train = processed_data_set$result_factor, 
+      alpha.a = finalParameters$alpha[i],
+      s_lambda.a = finalParameters$lambda[i], 
+      calibrate = FALSE)
+    
+    predictions[[i]] <- model$predictions
+    var_imp[[i]] <- model$variable_importance
     
   }
   
-  finalPredictions.processed = predictions %>% reduce(cbind) %>% rowMeans(.)
-  finalVarImp.processed = varImp %>% reduce(left_join, by = "Variable") %>% processVarImp(.)
+  final_predictions_processed <- predictions %>%
+    reduce(cbind) %>% 
+    rowMeans(.)
   
-  list(Prediction = finalPredictions.processed, Variable.Importance = finalVarImp.processed)
+  final_var_imp_processed <- var_imp %>% 
+    reduce(left_join, by = "variable") %>%
+    process_var_imp(.)
+  
+  list(prediction = final_predictions_processed, variable_importance = final_var_imp_processed)
 }
 
 #...........................Global.........................................#
 
 set.seed(40689)
-innerFolds = caret::createMultiFolds(y = allData$ResultProper, k = 3, times = 5)
+inner_folds <- caret::createMultiFolds(y = all_data$result_factor, k = 3, times = 5)
 
 #...............If you want to retrain the model, uncomment this and run.................#
 
-finalParameters = mclapply(X = 1:5, FUN = trainModel.oneRep, innerFolds = innerFolds, mainTrain = allData) %>%
+final_parameters <- mclapply(X = 1:5, FUN = train_model_one_rep, inner_folds = inner_folds, main_train = all_data) %>%
   bind_rows(.)
 
-saveRDS(finalParameters, file = "finalParameters.rds")
+saveRDS(final_parameters, file = "src/prediction/rds/final-parameters.rds")
 
-processedData = preProcessData(data = allData)
-saveRDS(processedData, file = "processedData.rds")
+processed_data <- pre_process_data(data = all_data)
+saveRDS(processed_data, file = "src/prediction/rds/processed-data.rds")
 
 #.................Otherwise, run this instead..................................#
 
@@ -157,10 +142,15 @@ saveRDS(processedData, file = "processedData.rds")
 
 #.................Find, and then present final predictions....................................#
  
-predictions = predict.NHL(processedDataSet = processedData$Data, recipeParameters = processedData$recipeParameters, newdata = newdata, finalParameters = finalParameters)
+predictions <- predict_NHL(
+  processed_data_set = processed_data$data,
+  recipe_parameters = processed_data$recipe_parameters, 
+  new_data = new_data, 
+  final_parameters = final_parameters)
 
-template = read_csv("/home/brayden/GitHub/NHLPlayoffs/Scraping Scripts and Template/Template.csv") %>%
+template <- read_csv("src/scraping/templates/template.csv") %>%
   filter(Year == 2019, Round == "stanley-cup-final") %>%
   select(Team1, Team2, Highest.Seed)
 
-finalScores = template %>% bind_cols(., Prob.Win.HighestSeed = predictions$Prediction)
+final_scores <- template %>%
+  bind_cols(., prob_win_highest_seed = predictions$prediction)
